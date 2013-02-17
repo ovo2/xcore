37176ad6fe435e899712224f500f720e4328130e
RexDude <rexdude@arcor.de>
2013-02-17 10:09:36 +0100

    New setting: drop_dbfields_on_uninstall #2

--- uninstall.inc.php
+++ uninstall.inc.php
@@ -14,9 +14,13 @@
  * @version 1.5.0
  */
 
-$sql = new rex_sql();
-//$sql->debugsql = true;
-$sql->setQuery('ALTER TABLE `' . $REX['TABLE_PREFIX'] . 'article` DROP `seo_title`, DROP `seo_description`, DROP `seo_keywords`, DROP `seo_url`, DROP `seo_noindex`, DROP `seo_ignore_prefix`');
+if ($REX["ADDON"]["rexseo42"]["settings"]['drop_dbfields_on_uninstall']) {
+	$sql = new rex_sql();
+	//$sql->debugsql = true;
+	$sql->setQuery('ALTER TABLE `' . $REX['TABLE_PREFIX'] . 'article` DROP `seo_title`, DROP `seo_description`, DROP `seo_keywords`, DROP `seo_url`, DROP `seo_noindex`, DROP `seo_ignore_prefix`');
+} else {
+	echo rex_info('Die Datenbankfelder wurden NICHT entfernt.');
+}
 
 rex_generateAll();
 
