81ae5e3552b1b7f456a459c047804ac5fddda64b
RexDude <rexdude@arcor.de>
2013-10-16 13:43:35 +0200

    Removed welcome link to setup

--- pages/welcome.inc.php
+++ pages/welcome.inc.php
@@ -1,5 +1,5 @@
 
-<p class="welcome"><a href="index.php?page=seo42&subpage=setup"><img src="../<?php echo $REX['MEDIA_ADDON_DIR']; ?>/seo42/dont_panic.png" width="240" height="308" alt="" /></a></p>
+<p class="welcome"><img src="../<?php echo $REX['MEDIA_ADDON_DIR']; ?>/seo42/dont_panic.png" width="240" height="308" alt="" /></p>
 
 <style type="text/css">
 #rex-page-seo42 p.welcome {
