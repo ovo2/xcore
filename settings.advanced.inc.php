332b88797cbcf3db3d9506f2208956fdc56da2b9
RexDude <rexdude@arcor.de>
2014-01-30 19:29:54 +0100

    smart_redirects enabled

--- settings.advanced.inc.php
+++ settings.advanced.inc.php
@@ -35,7 +35,7 @@ $REX['ADDON']['seo42']['settings']['all_url_types'] = true;
 $REX['ADDON']['seo42']['settings']['full_urls'] = false;
 
 // if true smart redirects like domain.de/foo/bar/ -> domain.de/foo/bar.html etc. will be enabled. default is false at it is still a experimental feature
-$REX['ADDON']['seo42']['settings']['smart_redirects'] = false;
+$REX['ADDON']['seo42']['settings']['smart_redirects'] = true;
 
 // default title delimiter (including whitespace chars) for seperating name of website and page title
 $REX['ADDON']['seo42']['settings']['title_delimiter'] = '-';
