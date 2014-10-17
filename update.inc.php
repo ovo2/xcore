5c78f843d3a592c81df78afdee835ecb01e19b02
RexDude <rexdude@arcor.de>
2014-10-17 13:49:19 +0200

    Update localized

--- update.inc.php
+++ update.inc.php
@@ -1,5 +1,11 @@
 <?php
 
-echo rex_info('SEO42: Bitte beachten Sie die <a href="index.php?page=seo42&subpage=help&chapter=update">Update-Hinweise</a> für diese Version.');
+if ($I18N->hasMsg('seo42_update_msg')) {
+	$msg = $I18N->msg('seo42_update_msg');
+} else {
+	$msg = 'SEO42: Bitte beachten Sie die <a href="index.php?page=seo42&subpage=help&chapter=update">Update-Hinweise</a> für diese Version.';
+}
+
+echo rex_info($msg);
 
 $REX['ADDON']['update']['seo42'] = 1;
