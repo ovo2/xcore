a20e55dd93e465b7ebf5b3803e53e31ec2d9285a
RexDude <rexdude@arcor.de>
2014-10-19 10:45:02 +0200

    langpreset prio changed

--- langpresets.inc.php
+++ langpresets.inc.php
@@ -2,6 +2,22 @@
 
 $REX['SEO42_LANG_PRESETS'] = array(
 	0 => array(
+		'code' => 'cz',
+		'original_name' => 'české',
+		'rewrite_mode' => SEO42_REWRITEMODE_SPECIAL_CHARS,
+		'special_chars' => 'À|à|Č|č|ć|È|è|É|é|Ì|ì|Í|í|Ï|ï|Ł|ł|Ò|ò|Ó|ó|Ù|ù|Ú|ú|ź|ż|ž|&',
+		'special_chars_rewrite' => 'A|a|C|c|c|E|e|E|e|I|i|I|i|I|i|L|l|O|o|O|o|U|u|U|u|z|z|z|a',
+		'inherit_from_clang' => 0
+	),
+	1 => array(
+		'code' => 'da',
+		'original_name' => 'dansk',
+		'rewrite_mode' => SEO42_REWRITEMODE_SPECIAL_CHARS,
+		'special_chars' => 'Å|å|Æ|æ|Ø|ø|É|é|Á|á|&',
+		'special_chars_rewrite' => 'Aa|aa|Ae|ae|Oe|oe|E|e|A|a|og',
+		'inherit_from_clang' => 0
+	),
+	2 => array(
 		'code' => 'de',
 		'original_name' => 'deutsch',
 		'rewrite_mode' => SEO42_REWRITEMODE_SPECIAL_CHARS,
@@ -9,7 +25,7 @@ $REX['SEO42_LANG_PRESETS'] = array(
 		'special_chars_rewrite' => 'Ae|ae|Oe|oe|Ue|ue|ss|und',
 		'inherit_from_clang' => 0
 	),
-	1 => array(
+	3 => array(
 		'code' => 'en',
 		'original_name' => 'english',
 		'rewrite_mode' => SEO42_REWRITEMODE_SPECIAL_CHARS,
@@ -17,28 +33,12 @@ $REX['SEO42_LANG_PRESETS'] = array(
 		'special_chars_rewrite' => 'and',
 		'inherit_from_clang' => 0
 	),
-	2 => array(
+	4 => array(
 		'code' => 'es',
 		'original_name' => 'español',
 		'rewrite_mode' => SEO42_REWRITEMODE_SPECIAL_CHARS,
 		'special_chars' => 'Á|á|ç|É|é|Í|í|Ñ|ñ|Ó|ó|Ú|ú|ü|&',
 		'special_chars_rewrite' => 'A|a|c|E|e|I|i|N|n|O|o|U|u|u|y',
-		'inherit_from_clang' => 0
-	),
-	3 => array(
-		'code' => 'it',
-		'original_name' => 'italiano',
-		'rewrite_mode' => SEO42_REWRITEMODE_SPECIAL_CHARS,
-		'special_chars' => 'À|à|È|è|É|é|Ì|ì|Í|í|Ï|ï|Ò|ò|Ó|ó|Ù|ù|Ú|ú|&',
-		'special_chars_rewrite' => 'A|a|E|e|E|e|I|i|I|i|I|i|O|o|O|o|U|u|U|u|e',
-		'inherit_from_clang' => 0
-	),
-	4 => array(
-		'code' => 'da',
-		'original_name' => 'dansk',
-		'rewrite_mode' => SEO42_REWRITEMODE_SPECIAL_CHARS,
-		'special_chars' => 'Å|å|Æ|æ|Ø|ø|É|é|Á|á|&',
-		'special_chars_rewrite' => 'Aa|aa|Ae|ae|Oe|oe|E|e|A|a|og',
 		'inherit_from_clang' => 0
 	),
 	5 => array(
@@ -50,11 +50,11 @@ $REX['SEO42_LANG_PRESETS'] = array(
 		'inherit_from_clang' => 0
 	),
 	6 => array(
-		'code' => 'pl',
-		'original_name' => 'polska',
+		'code' => 'it',
+		'original_name' => 'italiano',
 		'rewrite_mode' => SEO42_REWRITEMODE_SPECIAL_CHARS,
-		'special_chars' => 'À|à|Č|č|ć|È|è|É|é|Ì|ì|Í|í|Ï|ï|Ł|ł|Ò|ò|Ó|ó|Ù|ù|Ú|ú|ź|ż|ž|&',
-		'special_chars_rewrite' => 'A|a|C|c|c|E|e|E|e|I|i|I|i|I|i|L|l|O|o|O|o|U|u|U|u|z|z|z|i',
+		'special_chars' => 'À|à|È|è|É|é|Ì|ì|Í|í|Ï|ï|Ò|ò|Ó|ó|Ù|ù|Ú|ú|&',
+		'special_chars_rewrite' => 'A|a|E|e|E|e|I|i|I|i|I|i|O|o|O|o|U|u|U|u|e',
 		'inherit_from_clang' => 0
 	),
 	7 => array(
@@ -66,11 +66,11 @@ $REX['SEO42_LANG_PRESETS'] = array(
 		'inherit_from_clang' => 0
 	),
 	8 => array(
-		'code' => 'cz',
-		'original_name' => 'české',
+		'code' => 'pl',
+		'original_name' => 'polska',
 		'rewrite_mode' => SEO42_REWRITEMODE_SPECIAL_CHARS,
 		'special_chars' => 'À|à|Č|č|ć|È|è|É|é|Ì|ì|Í|í|Ï|ï|Ł|ł|Ò|ò|Ó|ó|Ù|ù|Ú|ú|ź|ż|ž|&',
-		'special_chars_rewrite' => 'A|a|C|c|c|E|e|E|e|I|i|I|i|I|i|L|l|O|o|O|o|U|u|U|u|z|z|z|a',
+		'special_chars_rewrite' => 'A|a|C|c|c|E|e|E|e|I|i|I|i|I|i|L|l|O|o|O|o|U|u|U|u|z|z|z|i',
 		'inherit_from_clang' => 0
 	),
 	9 => array(
