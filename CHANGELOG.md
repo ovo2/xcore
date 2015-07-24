32fe20732037352ad97c75ad7d1f7d8487974488
RexDude <rexdude@arcor.de>
2015-07-24 15:27:19 +0200

    Plugin page missing

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,7 @@ SEO42 - Changelog
 
 * Fixed #175: Bei mehrsprachigen Websites wurde der 404 Artikel nicht angezeigt wenn die `clang` IDs nicht durchgehend nummeriert waren, thx@GeForce92
 * Fixed #174: nav42: Breadcrumb Navigation zeigt jetzt keine offline Artikel mehr an
+* Fixed: Die Seite eines Plugins wurden nicht automatisch angezeigt im Menü
 * Neu: FAQ Eintrag (+ Startguide und Readme) hinzugefügt: "Auf meinem 1und1 Server werden die Image Manager Bilder nicht gecachet sonder jedesmal neu heruntergeladen?!" 
 
 ### Version 4.1.2 - 04. Mai 2015
--- config.inc.php
+++ config.inc.php
@@ -207,7 +207,7 @@ if ($REX['REDAXO']) {
 		$plugins = OOPlugin::getAvailablePlugins('seo42');
 
 		for ($i = 0; $i < count($plugins); $i++) {
-            if (file_exists($REX['INCLUDE_PATH'] . '/addons/seo42/plugins/' . $plugins[$i] . '/pages/' . $plugins[$i]) ) {
+            if (file_exists($REX['INCLUDE_PATH'] . '/addons/seo42/plugins/' . $plugins[$i] . '/pages/' . $plugins[$i] . '.inc.php') ) { 
 				$I18N->appendFile($REX['INCLUDE_PATH'] . '/addons/seo42/plugins/' . $plugins[$i] . '/lang/'); // make msg for subpage available at this point 
 				array_push($REX['ADDON']['seo42']['SUBPAGES'], array($plugins[$i], $I18N->msg('seo42_' . $plugins[$i])));
             }
