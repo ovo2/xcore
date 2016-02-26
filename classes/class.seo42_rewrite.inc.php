a4feb81fb9c7139c6bb868845d64fbe2c01ec919
RexDude <rexdude@arcor.de>
2016-02-26 19:11:27 +0100

    PHP7 support

--- classes/class.seo42_rewrite.inc.php
+++ classes/class.seo42_rewrite.inc.php
@@ -2,8 +2,14 @@
 
 class SEO42Rewrite
 {
-  
-  /* constructor */
+  // this is the new style constructor used by newer php versions.
+  // important: if you change the signatur of this method, change also the signature of SEO42Rewrite()
+  function __construct() {
+    $this->SEO42Rewrite();
+  }
+
+  // this is the deprecated old style constructor kept for compat reasons. 
+  // important: if you change the signatur of this method, change also the signature of __construct()
   function SEO42Rewrite() {
     // do nothing
   }
