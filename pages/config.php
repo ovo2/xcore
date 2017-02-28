acd1f08a4744000bda18a23d09bf99fe757564bc
RexDude <rexdude@arcor.de>
2017-02-28 02:43:37 +0100

    delete cache hint

--- pages/config.php
+++ pages/config.php
@@ -1,7 +1,7 @@
 <?php
 
 $content = '<h2>' . $this->i18n('config_object') . '</h2>';
-$content .= '<p>Bitte momentan noch über <code>rex_config::set("xcore", "the_key", "my_value");</code> einmalig (!) von Hand einstellen.</p>';
+$content .= '<p>Bitte momentan noch über <code>rex_config::set("xcore", "the_key", "my_value");</code> einmalig (!) von Hand einstellen. Danach bitte den Cache löschen.</p>';
 $content .= '<code><pre>' . print_r($this->getConfig(), true) . '</pre></code>';
 
 $fragment = new rex_fragment();
