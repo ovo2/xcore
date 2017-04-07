88bbab7755f9b782f72be420dd437fdfc6c4f095
RexDude <rexdude@arcor.de>
2017-04-07 20:33:14 +0200

    frontend_form: added labels

--- code/frontend_form/form_fields.php
+++ code/frontend_form/form_fields.php
@@ -9,21 +9,21 @@ if (rex::isBackend()) {
 		$rc = [];
 
 		$fields = [];
-		$fields[] = ['name' => 'email_to', 'value' => '', 'type' => REX_GLOBAL_SETTINGS_FIELD_TEXT];
-		$fields[] = ['name' => 'form_name', 'value' => 'Name', 'type' => REX_GLOBAL_SETTINGS_FIELD_TEXT];
-		$fields[] = ['name' => 'form_email', 'value' => 'E-Mail', 'type' => REX_GLOBAL_SETTINGS_FIELD_TEXT];
-		$fields[] = ['name' => 'form_phone', 'value' => 'Telefon', 'type' => REX_GLOBAL_SETTINGS_FIELD_TEXT];
-		$fields[] = ['name' => 'form_message', 'value' => 'Ihre Nachricht', 'type' => REX_GLOBAL_SETTINGS_FIELD_TEXT];
-		$fields[] = ['name' => 'form_robotron', 'value' => 'Bitte lassen Sie dieses Feld leer!', 'type' => REX_GLOBAL_SETTINGS_FIELD_TEXT];
-		$fields[] = ['name' => 'form_submit', 'value' => 'Senden', 'type' => REX_GLOBAL_SETTINGS_FIELD_TEXT];
-		$fields[] = ['name' => 'form_required_fields', 'value' => 'Pflichtfelder', 'type' => REX_GLOBAL_SETTINGS_FIELD_TEXT];
-		$fields[] = ['name' => 'form_not_valid_msg', 'value' => 'Bitte füllen Sie alle Pflichtfelder aus!', 'type' => REX_GLOBAL_SETTINGS_FIELD_TEXT];
-		$fields[] = ['name' => 'mail_success_msg', 'value' => 'Vielen Dank für Ihre Anfrage. Wir melden uns so bald wie möglich bei Ihnen.', 'type' => REX_GLOBAL_SETTINGS_FIELD_TEXT];
-		$fields[] = ['name' => 'mail_error_msg', 'value' => 'Leider ist ein Fehler aufgetreten. Bitte versuchen Sie es später wieder.', 'type' => REX_GLOBAL_SETTINGS_FIELD_TEXT];
+		$fields[] = ['label' => 'E-Mail Empfänger', 'name' => 'email_to', 'value' => '', 'type' => REX_GLOBAL_SETTINGS_FIELD_TEXT];
+		$fields[] = ['label' => '', 'name' => 'form_name', 'value' => 'Name', 'type' => REX_GLOBAL_SETTINGS_FIELD_TEXT];
+		$fields[] = ['label' => '', 'name' => 'form_email', 'value' => 'E-Mail', 'type' => REX_GLOBAL_SETTINGS_FIELD_TEXT];
+		$fields[] = ['label' => '', 'name' => 'form_phone', 'value' => 'Telefon', 'type' => REX_GLOBAL_SETTINGS_FIELD_TEXT];
+		$fields[] = ['label' => '', 'name' => 'form_message', 'value' => 'Ihre Nachricht', 'type' => REX_GLOBAL_SETTINGS_FIELD_TEXT];
+		$fields[] = ['label' => '', 'name' => 'form_robotron', 'value' => 'Bitte lassen Sie dieses Feld leer!', 'type' => REX_GLOBAL_SETTINGS_FIELD_TEXT];
+		$fields[] = ['label' => '', 'name' => 'form_submit', 'value' => 'Senden', 'type' => REX_GLOBAL_SETTINGS_FIELD_TEXT];
+		$fields[] = ['label' => '', 'name' => 'form_required_fields', 'value' => 'Pflichtfelder', 'type' => REX_GLOBAL_SETTINGS_FIELD_TEXT];
+		$fields[] = ['label' => '', 'name' => 'form_not_valid_msg', 'value' => 'Bitte füllen Sie alle Pflichtfelder aus!', 'type' => REX_GLOBAL_SETTINGS_FIELD_TEXT];
+		$fields[] = ['label' => '', 'name' => 'mail_success_msg', 'value' => 'Vielen Dank für Ihre Anfrage. Wir melden uns so bald wie möglich bei Ihnen.', 'type' => REX_GLOBAL_SETTINGS_FIELD_TEXT];
+		$fields[] = ['label' => '', 'name' => 'mail_error_msg', 'value' => 'Leider ist ein Fehler aufgetreten. Bitte versuchen Sie es später wieder.', 'type' => REX_GLOBAL_SETTINGS_FIELD_TEXT];
 
 		foreach ($fields as $field) {
 			// create fields
-			$rc[$field['name']] = rex_global_settings_add_field('', 'glob_' . $field['name'], $prio++, '', $field['type'], '');
+			$rc[$field['name']] = rex_global_settings_add_field($field['label'], 'glob_' . $field['name'], $prio++, '', $field['type'], '');
 
 			// add values
 			$sql = rex_sql::factory();
